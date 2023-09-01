7da115f3df447f7630699ab60acc0cd61ded3675
CoderStephen <>
2023-09-01 02:31:11 +0000

    got the function working!! could not figure out how to make an image a button, investigate further

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const playerChoice = document.getElementById('player-choice');
 const compChoice = document.getElementById('comp-choice');
 const outcome = document.getElementById('result');
-const choices = document.getElementsByClassName('options');
+const choices = document.querySelectorAll('button');
 let playChoice;
 choices.forEach(choices => choices.addEventListener('click', (event) => {
     playChoice = event.target.id;
     playerChoice.innerHTML = playChoice;
+    genCompChoice()
 }));
+function genCompChoice() {
+    randomNumber = math.floor(math.random() * choices.length)
+    console.log(randomNumber)
+}
