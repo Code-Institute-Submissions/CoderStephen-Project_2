4aca7fed991ca2b8bf26b08607ee1de33df1ba7d
CoderStephen <>
2023-09-02 01:51:46 +0000

    FINALLY got the java code working!!!!

--- app.js
+++ app.js
@@ -3,12 +3,30 @@ const compChoice = document.getElementById('comp-choice');
 const outcome = document.getElementById('result');
 const choices = document.querySelectorAll('button');
 let playChoice;
+let computerPlays;
+
 choices.forEach(choices => choices.addEventListener('click', (event) => {
-    playChoice = event.target.id;
-    playerChoice.innerHTML = playChoice;
-    genCompChoice()
+    playChoice = event.target.id
+    playerChoice.innerHTML = playChoice
+    generateCompChoice()
 }));
-function genCompChoice() {
-    randomNumber = math.floor(math.random() * choices.length)
-    console.log(randomNumber)
+function generateCompChoice() {
+   const randomNumber = Math.floor(Math.random() * choices.length) + 1
+    
+    if (randomNumber === 1){
+        computerPlays = 'rock';
+    } 
+    if (randomNumber === 2){
+        computerPlays = 'paper';
+    } 
+    if (randomNumber === 3) {
+            computerPlays = 'scissors';
+        } 
+        if (randomNumber === 4) {
+                computerPlays = 'lizard';
+            } 
+            if (randomNumber === 5) {
+                    computerPlays = 'spock';
+                }
+                compChoice.innerHTML = computerPlays;
 }
